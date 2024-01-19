6c60461f5e5b5827c78cc3ae71db45820382791d
spano <spano@student.chalmers.se>
2024-01-19 12:12:30 +0100

    Add `main.c` (template), assignment description

---
+++ main.c
@@ -0,0 +1,19 @@
+// (C) Erik Lindstrand, Konstantinos Rokanas, Michal Spano, group: 5 (2024)
+// Work package 1
+// Exercises x, y, z, ...
+// Submission code: <XXXYYY>
+
+// Include section
+#include <stdio.h>
+
+// Macro section
+// ...
+
+// Function prototype declaration(s)
+// ...
+
+// Main program section
+int main(int argc, char* argv[]) {
+  printf("WP1: TODO\n");
+  return 0;
+}
