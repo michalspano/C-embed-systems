69d21ac05e5ff959c32bfb62a5546c5108d2dbc0
spano <spano@student.chalmers.se>
2024-02-06 12:52:52 +0100

    Add submission code to `ex4`

--- ex4/decode.c
+++ ex4/decode.c
@@ -1,7 +1,7 @@
 // (C) Erik Lindstrand, Konstantinos Rokanas, Michal Spano, group: 5 (2024)
 // Work package 2
 // Exercises 4
-// Submission code: <XXXYYY>
+// Submission code: 5129972
 
 // Include section
 #include <stdio.h>
