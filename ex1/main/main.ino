bfab127c00783f0ccc7cbf14263e4b65895ede0c
spano <spano@student.chalmers.se>
2024-02-12 14:38:01 +0100

    Use non-blocking `millis()` in `ex1`

--- ex1/main/main.ino
+++ ex1/main/main.ino
@@ -3,36 +3,50 @@
 // Exercise 1
 // Submission code: <XXXYYY>
 
-const uint8_t PIN_OUT[]	 = { 4, 7 }; // Declare input pins
+uint8_t button_is_on     = 0,        // Store the state of the push button (on/off)
+        LED_is_on        = 0;        // Store the state of the LED (on/off)
+const uint8_t d_interval = 1000;     // Declare an interval delay (1s)
 const uint8_t PIN_IN[]   = { 2 };    // Declare output pin
-const uint8_t d_interval = 500;      // Declare an interval delay
-int button_state         = 0;        // Store the state of the push button
+const uint8_t PIN_OUT[]  = { 4, 7 }; // Declare input pins
+unsigned long t_buff     = 0;        // Time indicator buffer (used with `millis`)
 
 /* Set up the individual pins */
 void setup() {
-  pinMode(PIN_IN[0],  INPUT);  // Push button at digital pin 2. 
+  pinMode(PIN_IN[0],  INPUT);  // Push button at digital pin 2.
   pinMode(PIN_OUT[0], OUTPUT); // LED at pin 4.
   pinMode(PIN_OUT[1], OUTPUT); // LED at pin 7.
 }
 
 /* Program's loop body */
 void loop() {
-  // Note: this blocks the button reading. Preferably, it should be extracted
-  // to a stand-alone construct. Solution: use non-blocking millis().
-  // Source: https://arduinogetstarted.com/faq/how-to-use-millis-instead-of-delay.
-  // FIXME: use millis() instead of delay().
+  // During each iteration of the loop, get the current elapsed time of the
+  // program. This implementation uses the non-blocking `millis()` function.
+  unsigned long current_time = millis();
 
-  digitalWrite(PIN_OUT[1], HIGH); // Light up LED
-  delay(d_interval);              // Wait 0.5s
-  digitalWrite(PIN_OUT[1], LOW);  // Dim the LED
-  delay(d_interval);              // Wait 0.5s
- 
+  // The following code block will be executed each 1 second.
+  if (current_time > t_buff + d_interval) {
+    t_buff = current_time;  // Assign the current time to the buffer.
+    LED_is_on = !LED_is_on; // Negate the state of the LED (is should
+                            // alternate every second).
+
+    // Based on the state of the LED, either turn it on or off.
+    LED_is_on ? digitalWrite(PIN_OUT[1], LOW)    // Dim the LED
+              : digitalWrite(PIN_OUT[1], HIGH);  // Light up the LED
+  }
+
+  // The following code segment is executed separately, because `millis()` is
+  // non-blocking.
+
   // Read the state of the button at digital port 2.
-  button_state = digitalRead(PIN_IN[0]);
+  button_is_on = digitalRead(PIN_IN[0]);
 
   // If the button is pressed, light up the LED. Otherwise, dim the LED if the
   // button is not pressed.
-  button_state == HIGH ? digitalWrite(PIN_OUT[0], HIGH)
+  button_is_on == HIGH ? digitalWrite(PIN_OUT[0], HIGH)
                        : digitalWrite(PIN_OUT[0], LOW);
-  delay(10); // Add some small delay
+
+  // Add some small delay. This value is minimal, so it is acceptable that the blocking
+  // `delay` function is used.
+  delay(10);
 }
+
