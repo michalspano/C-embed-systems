b9769e5fd8c5fd70b20619ee4673409d83c8005b
spano <spano@student.chalmers.se>
2024-02-12 15:35:14 +0100

    Rename ex5 -> ex6, ex6 -> ex7
    The numbers are a bit confusing in the assignment, yes.

--- ex6/main.c
+++ ex6/main.c
@@ -1,98 +1,74 @@
 // (C) Erik Lindstrand, Konstantinos Rokanas, Michal Spano, group: 5 (2024)
 // Work package 3
-// Exercise 6
+// Exercise 5
 // Submission code: <XXXYYY>
 
 // Includes section
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Macros section
-#define STR_SIZE 20
+#define MAX 20     // Number of ints in the buffer array.
+#define MAX_VAL 99 // Maximum allowed integer placed in the buffer.
 
 // Function prototypes
-void clearBuffer();                     // Consume remaining chars.
-void copyString(char* buff, char* str); // Custom alias of `strcpy`.
+int random_int();  // Compute a random int in the desired range.
 
 /**
  * Main program section. The program does not use any command-line arguments
  * provided when the executable is run.
  */
 int main(void) {
-  // The string is to be of size 20 chars. Instantiate 3 buffers: (i) for the
-  // input string, (ii) for the string copied with `strcpy`, (iii) for the
-  // string copied with the custom function. Add +1 to include the null
-  // terminator.
-  char in_str[STR_SIZE + 1],
-           s1[STR_SIZE + 1], 
-           s2[STR_SIZE + 1];
+  srand(time(NULL));   // Reset the seed.
+  int array[MAX];      // Declare integer array of size MAX.
+  int *pArray = array; // Store the pointer to MAX in a stand-alone variable.
+                       // This is used for pointer incrementation.
 
-  // Prompt the user to provide the string.
-  printf("Provide a string: ");
+  // Populate the array, use the `random_int()` function to generate the
+  // individual integer values.
+  for (int i = 0; i < MAX; i++) array[i] = random_int();
 
-  // Use a width specifier of 20 chars.
-  if (scanf("%20s", in_str)) {
-    // If reading was successful, add the null terminator at the end and clear
-    // any remaining chars in the buffer.
-    in_str[STR_SIZE] = '\0'; clearBuffer();
-  } else {
-    // Otherwise, notify the user about the error and exit the program.
-    printf("Error while reading the input.\n");
-    return 1;
+  // 1. The value of the address of the array
+  printf("The value of the address of the array (pointer) is: %p\n", array);
+
+  // 2. First integer in the array
+  // We can simply extract the value of the first integer in the array by
+  // dereferencing the pointer. Alternatively we can use array[0] to access the
+  // first element.
+  printf("First integer in the array is (array[0]) %d\n", *pArray);
+
+  // 3. The last integer
+  // We can extract the value of the last integer in the array by adding the
+  // number of elements in the array to the pointer and then dereferencing it.
+  // Alternatively, we can use array[MAX - 1] to access the last element.
+  printf("The last integer in the array is: %d\n", *(pArray + MAX - 1));
+
+  // 4. Size of an int
+  printf("The size of an integer (number of bytes) is: %lu\n", sizeof(int));
+
+  // 5. The size of the whole array
+  printf("The size of the whole array in byte is: %lu\n\n", MAX * sizeof(int));
+
+  // 6. Print out each value from the int buffer and multiply it by two.
+  for (int i = 0; i < MAX; i++) {
+    int val = *pArray++; // Extract the value of the address, increment the
+                         // pointer (to point to the next value in the
+                         // sequence).
+    // Print each line in a formatted manner.
+    printf("array[%d] = %d, array[%d] * 2 = %d\n", i, val, i, val * 2);
   }
 
-  // Use the `strcpy` function to copy the contents of the string to `s1`.
-  strcpy(s1, in_str);
-
-  // Use the custom function to copy the contents of the string to `s2`.
-  copyString(s2, in_str);
-
-  // 'Manual' check that verifies that the copied strings are identical. That is,
-  // assuming some string s, strcpy(s) == copyString(s). Change #if 0 to #if 1 for
-  // the pre-processor to recognize the code segment.
-#if 0
-  // Verify that the strings are equal. Use the `strcmp` function. Assume that
-  // `strcpy` always yields, in this case, a correct result.
-  _Bool are_identical = strcmp(s1, s2) == 0;
-
-  // Print the result.
-  are_identical ? printf("The strings are identical.\n")
-                : printf("The strings are NOT identical.\n");
-#endif
-
-  // Print the string to the console.
-  printf("The processed string: %s\n", s2);
-
-  // Exit with 0 to indicate a successful completion.
+  // Everything went well, return 0.
   return 0;
 }
 
 /**
- * Custom function to copy the contents of a string to another char buffer.
- * @param buff - the buffer where the chars are to be placed.
- * @param str  - the string to be copied.
- *
- * This function does not return a value, albeit causes a side effect and the
- * state of `buff` is updated.
+ * A helper function to generate a random integer in the range [1, 99].
+ * @returns - a new integer in the above interval.
  */
-void copyString(char* buff, char* str) {
-  char c; // Initialize c
-
-  // Attempt to read chars of str until the null-terminator is encountered.
-  // Each char is placed at a location of the buffer where the current pointer
-  // of buff points to. The pointers are consecutively incremented to denote.
-  while ((c = *str) != '\0') *buff++ = *str++;
-
-  // Ultimately, add the null terminator as the last element. We have to do
-  // this manually, because, previously, the null-terminator was used as an
-  // identifier to limit a char sequence.
-  *buff = '\0';
-}
-
-/**
- * A helper subroutine to consume remaining chars in the buffer.
- */
-void clearBuffer() {
-  // Get all chars until new-line or EOF are encountered.
-  char c; while ((c = getchar()) != '\n' && c != EOF);
+int random_int() {
+  // Use the `rand()` function with the modulo operator, use +1 to make the
+  // boundary values included in the interval.
+  return (rand() % MAX_VAL) + 1;
 }
---
+++ ex7/main.c
@@ -0,0 +1,98 @@
+// (C) Erik Lindstrand, Konstantinos Rokanas, Michal Spano, group: 5 (2024)
+// Work package 3
+// Exercise 6
+// Submission code: <XXXYYY>
+
+// Includes section
+#include <stdio.h>
+#include <string.h>
+
+// Macros section
+#define STR_SIZE 20
+
+// Function prototypes
+void clearBuffer();                     // Consume remaining chars.
+void copyString(char* buff, char* str); // Custom alias of `strcpy`.
+
+/**
+ * Main program section. The program does not use any command-line arguments
+ * provided when the executable is run.
+ */
+int main(void) {
+  // The string is to be of size 20 chars. Instantiate 3 buffers: (i) for the
+  // input string, (ii) for the string copied with `strcpy`, (iii) for the
+  // string copied with the custom function. Add +1 to include the null
+  // terminator.
+  char in_str[STR_SIZE + 1],
+           s1[STR_SIZE + 1], 
+           s2[STR_SIZE + 1];
+
+  // Prompt the user to provide the string.
+  printf("Provide a string: ");
+
+  // Use a width specifier of 20 chars.
+  if (scanf("%20s", in_str)) {
+    // If reading was successful, add the null terminator at the end and clear
+    // any remaining chars in the buffer.
+    in_str[STR_SIZE] = '\0'; clearBuffer();
+  } else {
+    // Otherwise, notify the user about the error and exit the program.
+    printf("Error while reading the input.\n");
+    return 1;
+  }
+
+  // Use the `strcpy` function to copy the contents of the string to `s1`.
+  strcpy(s1, in_str);
+
+  // Use the custom function to copy the contents of the string to `s2`.
+  copyString(s2, in_str);
+
+  // 'Manual' check that verifies that the copied strings are identical. That is,
+  // assuming some string s, strcpy(s) == copyString(s). Change #if 0 to #if 1 for
+  // the pre-processor to recognize the code segment.
+#if 0
+  // Verify that the strings are equal. Use the `strcmp` function. Assume that
+  // `strcpy` always yields, in this case, a correct result.
+  _Bool are_identical = strcmp(s1, s2) == 0;
+
+  // Print the result.
+  are_identical ? printf("The strings are identical.\n")
+                : printf("The strings are NOT identical.\n");
+#endif
+
+  // Print the string to the console.
+  printf("The processed string: %s\n", s2);
+
+  // Exit with 0 to indicate a successful completion.
+  return 0;
+}
+
+/**
+ * Custom function to copy the contents of a string to another char buffer.
+ * @param buff - the buffer where the chars are to be placed.
+ * @param str  - the string to be copied.
+ *
+ * This function does not return a value, albeit causes a side effect and the
+ * state of `buff` is updated.
+ */
+void copyString(char* buff, char* str) {
+  char c; // Initialize c
+
+  // Attempt to read chars of str until the null-terminator is encountered.
+  // Each char is placed at a location of the buffer where the current pointer
+  // of buff points to. The pointers are consecutively incremented to denote.
+  while ((c = *str) != '\0') *buff++ = *str++;
+
+  // Ultimately, add the null terminator as the last element. We have to do
+  // this manually, because, previously, the null-terminator was used as an
+  // identifier to limit a char sequence.
+  *buff = '\0';
+}
+
+/**
+ * A helper subroutine to consume remaining chars in the buffer.
+ */
+void clearBuffer() {
+  // Get all chars until new-line or EOF are encountered.
+  char c; while ((c = getchar()) != '\n' && c != EOF);
+}
