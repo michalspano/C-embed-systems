09d38ce8e05034bdbbc3aa4dc5fbd22367cd9085
spano <spano@student.chalmers.se>
2024-02-14 21:45:02 +0100

    Simplify repo structure

--- ex1/plug.sh
+++ ex1/plug.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# Upload sketch to the board.
+# Upload sketch to the board. Used when testing the sketch with the physical components.
 cd main; arduino-cli upload -p /dev/cu.usbmodemF412FA651FA82 --fqbn arduino:renesas_uno:unor4wifi
