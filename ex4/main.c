e4d7a0fd2dc8590056f8add78d4b5eb7ae852f78
spano <spano@student.chalmers.se>
2024-02-20 14:32:18 +0100

    Add micro-changes to `ex4`

--- ex4/main.c
+++ ex4/main.c
@@ -1,15 +1,15 @@
 // (C) Erik Lindstrand, Konstantinos Rokanas, Michal Spano, group: 5 (2024)
 // Work package 4
 // Exercise 4
-// Submission code: <XXXYYY>
+// Submission code: 939466
 
-#include <stdio.h>                                      // header for standard library containing functions for input and output
+#include <stdio.h>                                   // header for standard library containing functions for input and output
 
 // function declarations
-int searchNumber(int number, int tab[], int size);       // determines if int number exists in an array and at what index
-void sort(int number, int tab[], int length);           // sorts elements of array in numerical order
-void swapElements(int* a, int* b);                      // helper that swaps array elements pointed at by two pointers
-void printArray(char* prefix, int *tab, int length);    // helper that prints contents of array
+int searchNumber(int number, int tab[], int size);   // determines if int number exists in an array and at what index
+void sort(int number, int tab[]);                    // sorts elements of array in numerical order
+void swapElements(int* a, int* b);                   // helper that swaps array elements pointed at by two pointers
+void printArray(char* prefix, int *tab, int length); // helper that prints contents of array
 
 // main program logic
 
@@ -17,17 +17,19 @@ void printArray(char* prefix, int *tab, int length);    // helper that prints co
 * Main function executed by the program.
 *
 * @function main
-* @param int argc - number of user inputted arguments in command line
-* @param char *argv[] - array of pointers to Strings containing each argument (index 0 is always program name)
-* @return int - exit code, 0 == successful program execution and 1 == program termination with error
+* @param int argc     - number of user inputted arguments in command line
+* @param char *argv[] - array of pointers to Strings containing each argument
+*                       (index 0 is always program name)
+* @return int         - exit code, 0 == successful program execution and 1 ==
+*                       program termination with error
 **/
 int main (void) {
 
     // variable declarations
-    int n = 13;                                           // define an integer
-    int test[] = {1, 2, 34, 5, 67, 3, 23, 12, 13, 10};    // initialize an array of integers
-    int size = sizeof(test);                              // determine size of array (in bytes)
-    int length = size / sizeof(int);                      // determine length of array by dividing its size by the size of one elemnet
+    int n = 13;                                        // define an integer to find
+    int test[] = {1, 2, 34, 5, 67, 3, 23, 12, 13, 10}; // initialize an array of integers (taken from the example)
+    int size   = sizeof(test);                         // determine size of array (in bytes)
+    int length = size / sizeof(int);                   // determine length of array by dividing its size by the size of one elemnet
 
     // call helper function to print the unsorted array
     printArray("Unsorted array:", test, length);
@@ -36,11 +38,11 @@ int main (void) {
     // store index of first occurrence of n in variable
     int firstIndex = searchNumber(n, test, size);
 
-    // print resulting first occurence (-1 signifies number not present in String)    
-    printf("Index of n (%d): %d\n", n, firstIndex);
+    // print resulting first occurrence (-1 signifies number not present in String)    
+    printf("Index of n = %d: [%d]\n", n, firstIndex);
 
     // call function to sort test[] array in numerical order  
-    sort(n, test, length);
+    sort(length, test);
 
     // call helper function to print the sorted array
     printArray("Sorted array:", test, length);
@@ -50,13 +52,15 @@ int main (void) {
 }
 
 /**   
-* determine if number exists in an array and at what index
+* Determine if number exists in an array and at what index. Note that the first
+* occurrence is returned.
 * 
 * @function searchNumber
 * @param int number - number being searched for in array
-* @param int tab[] - unsorted array of numbers being searched through
-* @param int size - size of array (in bytes)
-* @return int - index of first occurrence of number in array (-1 if number is not present in array)
+* @param int tab[]  - unsorted array of numbers being searched through
+* @param int size   - size of array (in bytes)
+* @return int       - index of first occurrence of number in array (-1 if
+*                     number is not present in array)
 **/
 int searchNumber(int number, int tab[], int size) {
 
@@ -82,25 +86,24 @@ int searchNumber(int number, int tab[], int size) {
 * sorts elements of array in numerical order
 * 
 * @function sort
-* @param int number - I'M NOT ACTUALLY SURE WHAT THIS IS MEANT TO BE!!!
-* @param int* tab - pointer to the first element of array to be sorted
-* @param int length - number of elements in array  - TECHNICALLNY NOT PART OF ORIGINAL SIGNATURE!!!
+* @param int number - the number of elements in the array
+* @param int* tab   - pointer to the first element of array to be sorted
 **/
-void sort(int number, int tab[], int length) {
+void sort(int number, int tab[]) {
 
     // declare variable to store the index of the smallest element in array
     int minIndex;
 
     // loop through length of array
     // elements at index i will be compared against elements at index j
-    for(int i = 0; i < length - 1; i++) {
+    for(int i = 0; i < number - 1; i++) {
 
         // each loop, reset the smallest index to the new value of i
         minIndex = i;
 
         // loop through length of array
         // elements at index j will be compared against the current leftmost element (index i)
-        for(int j = i; j < length; j++) {
+        for(int j = i; j < number; j++) {
 
             // if the current element at index j is numerically smaller than the element at minIndex
             if(tab[j] < tab[minIndex]) {
@@ -124,8 +127,8 @@ void sort(int number, int tab[], int length) {
 **/
 void swapElements(int* a, int* b) { 
 
-    // declare temporary storage of a character  
-    char tempChar; 
+    // declare temporary storage of an int
+    int tempChar; 
 
     // set the value of tempChar to the value pointed at by pointer a
     tempChar = *a; 
@@ -142,8 +145,8 @@ void swapElements(int* a, int* b) {
 * 
 * @function printArray
 * @param char* prefix - String to be printed before the array
-* @param int* tab - pointer to the first element of array to be printed
-* @param int length - length of array to be printed
+* @param int* tab     - pointer to the first element of array to be printed
+* @param int length   - length of array to be printed
 **/
 void printArray(char* prefix, int *tab, int length) {
 
