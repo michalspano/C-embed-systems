afc1bbc33fcb52a67ce2539a123a8d750640960b
spano <spano@student.chalmers.se>
2024-03-03 19:25:47 +0100

    Polish `plug.sh`

--- ex3/plug.sh
+++ ex3/plug.sh
@@ -4,14 +4,12 @@
 # board (this is helpful when a more light-weight option than Arduino IDE is
 # used).
 
-SRC="./main"               # relative path to the source
-# SRC="./test"                 # relative path to the source [debug]
-BOARD="arduino:avr:uno"      # Plain Arduino Uno
+SRC="./main"                  # relative path to the source
+BOARD="arduino:avr:uno"       # Plain Arduino Uno
 PORT="/dev/cu.usbserial-1140" # At this destination (may vary per system)
 
 arduino-cli compile --fqbn $BOARD $SRC         # compile
 arduino-cli upload -p $PORT --fqbn $BOARD $SRC # upload
 
 # If -p flag is given, open the serial monitor
-if [ "$1" = "-p" ]; then cat $PORT
-fi
+if [ "$1" = "-p" ]; then cat $PORT; fi
